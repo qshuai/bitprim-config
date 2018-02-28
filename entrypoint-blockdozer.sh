68bba818a476a0aa738fe46a2d45d3c7bc97dfee
elfarto <arceri@pythian.com>
2018-02-28 15:17:38 -0500

    Add zcash scripts

--- entrypoint-blockdozer.sh
+++ entrypoint-blockdozer.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-[ ! -n "$CONFIG_REPO" ] && CONFIG_REPO=https://github.com/bitprim/bitprim-config.git
-[ ! -n "$ENTRYPOINT_SCRIPT" ] && ENTRYPOINT_SCRIPT=blockdozer-new.sh
+[ ! -n "$CONFIG_REPO" ] && CONFIG_REPO=git@github.com:Bitprim-Infra/blockdozer.git
+[ ! -n "$ENTRYPOINT_SCRIPT" ] && ENTRYPOINT_SCRIPT=entrypoint-blockdozer-new.sh
 
 echo "Cloning Config Repository ${CONFIG_REPO}"
 cd /root
+mkdir -p /root/.ssh
+echo "${SSH_KEY}" >.ssh/id_rsa
+chmod 600 .ssh/id_rsa
 rm -rf bitprim-config
-git clone https://github.com/bitprim/bitprim-config.git
+cat <<EOF >/root/.ssh/config 
+Host *
+StrictHostKeyChecking no 
+EOF
+cat /root/.ssh/id_rsa
+cat /root/.ssh/config
+git clone ${CONFIG_REPO} bitprim-config
 echo "Running entrypoint script ${ENTRYPOINT_SCRIPT}"
 . /root/bitprim-config/${ENTRYPOINT_SCRIPT}
+sleep 20000
