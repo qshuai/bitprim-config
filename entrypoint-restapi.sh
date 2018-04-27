418d083b4fbe49ecbb432b59b4216aebd79417da
elfarto <arceri@pythian.com>
2018-04-27 13:25:52 -0400

    Fixing Syntax

--- entrypoint-restapi.sh
+++ entrypoint-restapi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ ! -n "$CONFIG_REPO" ] && CONFIG_REPO=https://github.com/bitprim/bitprim-config.git
 [ ! -n "$ENTRYPOINT_SCRIPT" ] && ENTRYPOINT_SCRIPT=entrypoint-restapi-new.sh
-apt-get -y update && apt-get install git
+apt-get update && apt-get install -y git
 echo "Cloning Config Repository ${CONFIG_REPO}"
 cd /root
 rm -rf bitprim-config
