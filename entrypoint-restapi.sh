336988e60fb422d362b784935d51f0c0c740cad9
elfarto <arceri@pythian.com>
2018-04-27 11:34:50 -0400

    Creating scripts for REST-API

---
+++ entrypoint-restapi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+[ ! -n "$CONFIG_REPO" ] && CONFIG_REPO=https://github.com/bitprim/bitprim-config.git
+[ ! -n "$ENTRYPOINT_SCRIPT" ] && ENTRYPOINT_SCRIPT=entrypoint-restapi-new.sh
+
+echo "Cloning Config Repository ${CONFIG_REPO}"
+cd /root
+rm -rf bitprim-config
+git clone ${CONFIG_REPO} bitprim-config
+echo "Running entrypoint script ${ENTRYPOINT_SCRIPT}"
+. /root/bitprim-config/${ENTRYPOINT_SCRIPT}
+sleep 20000
