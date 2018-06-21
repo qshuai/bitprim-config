6d7229b2b574f770d64b4a728cf08a91b584440c
elfarto <arceri@pythian.com>
2018-06-21 12:38:20 -0400

    Change to npm install

--- entrypoint-blockdozer-wwwroot.sh
+++ entrypoint-blockdozer-wwwroot.sh
@@ -49,7 +49,8 @@ cd /bitprim/blockdozer/insight-ui
 if [ ! -x "./node_modules/.bin/grunt" ] ;then
 
 log "Installing grunt on /bitprim/blockdozer/insight-ui" 
-npm install "grunt" "grunt-cli" "grunt-css" "grunt-markdown" "grunt-macreload" "grunt-angular-gettext"  "grunt-contrib-uglify" "grunt-contrib-concat" "grunt-contrib-watch" "grunt-replace"
+#npm install "grunt" "grunt-cli" "grunt-css" "grunt-markdown" "grunt-macreload" "grunt-angular-gettext"  "grunt-contrib-uglify" "grunt-contrib-concat" "grunt-contrib-watch" "grunt-replace"
+npm install 
 fi
 
 
