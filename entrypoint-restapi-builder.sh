5cb41e3ea8b01248d8fca54554a3d050156f06fb
elfarto <arceri@pythian.com>
2018-08-15 17:26:49 -0400

    Add cpuid

--- entrypoint-restapi-builder.sh
+++ entrypoint-restapi-builder.sh
@@ -50,7 +50,7 @@ if [ ! -e /tmp/already_installed ] ; then
     apt-get update
     apt-get -y install cmake build-essential python-pip git $ADDITIONAL_PACKAGES
     apt-get -y remove python-pip
-    easy_install pip && pip install conan==${CONAN_VERSION} &&  conan remote add bitprim https://api.bintray.com/conan/bitprim/bitprim
+    easy_install pip && pip install conan==${CONAN_VERSION} cpuid &&  conan remote add bitprim https://api.bintray.com/conan/bitprim/bitprim
     if [ $? -eq 0 ] ; then
         echo "$ADDITIONAL_PACKAGES installed" >/tmp/already_installed
     fi
