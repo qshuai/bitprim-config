8ffad24e63715ffad7b38bfbbd8618ba2abcb683
elfarto <arceri@pythian.com>
2018-09-20 14:04:15 -0400

    Add build flag

--- entrypoint-bws.sh
+++ entrypoint-bws.sh
@@ -29,16 +29,26 @@ start_bws()
 {
 trap _term SIGTERM
 log "Starting bws"
+if [ -e /bitprim/bws/build_complete ] ; then
 cd /bitprim/bws && npm start && tail -f ${LOG_DIR}/*
+
 child=$!
-log "BWS Started PID=$child"
+log "Started dotnet process PID=$child"
 wait $child
+else
+log "build_complete flag not present, sleeping for 10 seconds and retrying "
+sleep 10
+start_bws
+fi
+
+
 }
 
 [ ! -n "${LOG_DIR}" ] && LOG_DIR=logs
 [ ! -n "${PID_DIR}" ] && PID_DIR=pids
 mkdir -p ${LOG_DIR} ${PID_DIR}
 
+sleep 5
 clean_bws
 start_bws
 sleep 300
