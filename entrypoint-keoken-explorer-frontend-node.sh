073f0db3ab632dab30355188056edd1ab6709ac2
elfarto <arceri@pythian.com>
2018-09-30 20:15:26 -0400

    New nginx entrypoint

--- entrypoint-keoken-explorer-frontend-node.sh
+++ entrypoint-keoken-explorer-frontend-node.sh
@@ -7,27 +7,46 @@ echo $(date +"%Y-%m-%d %H:%M:%S") $@
 
 
 
+configure_nginx()
+{
+cat<<EOF | envsubst >/etc/nginx/conf.d/default.conf
+
+    server {
+      root /bitprim/keoken-explorer-frontend/dist;
+      listen 80;
+      index index.html index.htm;
+      etag on;
+      client_max_body_size 16M;
+      keepalive_timeout 5;
+      location / {
+        index index.html;
+        try_files $uri /index.html;
+      }
+    }
+
+EOF
+}
+
 _term() {
   log "Caught SIGTERM signal!"
   log Waiting for $child
   kill -TERM "$child" ; wait $child 2>/dev/null
 }
 
-start_node()
+start_nginx()
 {
 trap _term SIGTERM
-log "Starting Application"
-cd /bitprim/keoken-explorer-frontend
-npm run build:prod
+log "Starting Nginx"
+nginx -g 'daemon off;' 
 child=$!
-log "NPM Started PID=$child"
+log "Nginx Started PID=$child"
 wait $child
 }
 
 
 
 configure_nginx
-start_node
-sleep 300
+start_nginx
+sleep 20000
 
 
