bf1f8bf4fb7ae367e953a80179d45ffe13ee15c1
elfarto <arceri@pythian.com>
2018-10-30 11:23:19 -0400

    reduce sleep on fail

--- entrypoint-zcash.sh
+++ entrypoint-zcash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 [ ! -n "$CONFIG_REPO" ] && CONFIG_REPO=https://github.com/bitprim/bitprim-config.git
 [ ! -n "$ENTRYPOINT_SCRIPT" ] && ENTRYPOINT_SCRIPT=entrypoint-zcash-new.sh
-
+[ ! -n "$SLEEP_TIME" ] && SLEEP_TIME=60
 echo "Cloning Config Repository ${CONFIG_REPO}"
 cd /root
 rm -rf bitprim-config
 git clone ${CONFIG_REPO} bitprim-config
 echo "Running entrypoint script ${ENTRYPOINT_SCRIPT}"
 . /root/bitprim-config/${ENTRYPOINT_SCRIPT}
-sleep 20000
+sleep ${SLEEP_TIME}
